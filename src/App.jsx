e934ea1d0d50f8b14f042fb4f5ec500c7af43060
Anil <anil6996f@gmail.com>
2024-06-04 12:36:47 +0530

    Completed the header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import CryptoHome from "./pages/CryptoHome"
 import CryptoDetail from "./pages/CryptoDetail"
+import Navbar from "./components/Navbar"
 
 function App() {
   
   return (
     <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<CryptoHome/>} />
         <Route path="/coin/:id" element={<CryptoDetail/>} />
