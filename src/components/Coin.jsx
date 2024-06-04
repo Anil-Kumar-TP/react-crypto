918ce55574d26ad17497ae6ed5057f1fe5a46a0b
Anil <anil6996f@gmail.com>
2024-06-04 17:22:54 +0530

    Completed the Markets Component

---
+++ src/components/Coin.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { TrendingDown, TrendingUp } from '../icons/icons';
+
+const Coin = ({ coin }) => {
+    return (
+        <>
+            <div className='grid grid-cols-3 sm:grid-cols-4 font-light p-2 rounded border-gray-200 border-b hover:bg-gray-400 cursor-pointer items-center'>
+                <div className='flex items-center gap-1 w-full'>
+                    <img src={coin.image} alt="" className='w-6' />
+                    <p>{coin.name}</p>
+                    <span className='text-xs'>({coin.symbol})</span>
+                </div>
+                <span className='w-full text-center'>${coin.current_price.toFixed(3)}</span>
+                <span className={`flex gap-1 ${coin.price_change_percentage_24h < 0 ? 'text-red-400' : 'text-green-400'}`}>{coin.price_change_percentage_24h < 0 ? <TrendingDown /> : <TrendingUp />} {coin.price_change_percentage_24h}</span>
+                <div className='hidden sm:block'>
+                    <p className='font-semibold'>Market Cap</p>
+                    <span>${coin.market_cap}</span>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Coin;
--- src/components/CoinTrending.jsx
+++ src/components/CoinTrending.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CoinTrending = ({ coin }) => {
     return (
         <>
-            <div className='font-light mb-2 p-2 bg-gray-200 border-2 rounded hover:bg-gray-400 cursor-pointer'>
+            <div className='font-light mb-2 p-2 border-gray-200 border-2 rounded hover:bg-gray-400 cursor-pointer'>
                 <div className='flex items-center gap-1'>
                     <span className='font-semibold'>{coin.score + 1}.</span>
                     <img src={coin.small} alt="" className='w-6'/>
